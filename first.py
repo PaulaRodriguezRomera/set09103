41b1762141bd162fc68c9208473a8ff1fa9614ea
Paula Rodriguez Romera <prodrig3@ed.ac.uk>
2023-09-26 19:28:20 +0100

    lab6 completed

--- first.py
+++ first.py
@@ -1,12 +1,12 @@
-from flask import Flask, url_for
+from flask import Flask, url_for, request
 app = Flask(__name__)
 
 @app.route('/')
 def root():
     return 'Default root'
 
-@app.route('/hello/')
-def hello():
+@app.route('/good-morning/')
+def good_morning():
     return 'Hello You!'
 
 @app.route('/goodbye/')
@@ -23,3 +23,57 @@ def static_example_img():
     url = url_for('static', filename='vmask.jpg')
     end = '">'
     return start+url+end, 200
+
+@app.route("/account/", methods=['GET', 'POST'])
+def account():
+    if request.method == 'POST':
+        print(request.form)
+        name = request.form['name']
+        return "Hello  %s" % name
+    else:
+        page = '''
+        <html><body>
+        <form action="" method="post" name="form">
+            <label for="name"> Name: </label>
+            <input type="text" name="name" id="name"/>
+            <input type="submit" name="submit" id="submit"/>
+        </form>
+        </body><html>'''
+
+        return page
+
+@app.route("/hello/<name>")
+def hello(name):
+    return "Hello %s" % name
+
+@app.route("/add/<int:first>/<int:second>")
+def add(first, second):
+    return str(first+second)
+
+@app.route('/parameters/')
+def parameters():
+    name = request.args.get('name', '')
+    if name == '':
+        return "no param supplied"
+    else:
+        return "Hello %s" % name
+
+@app.route("/upload/", methods=['POST', 'GET'])
+def upload():
+    if request.method == 'POST':
+        f = request.files['datafile']
+        f.save('static/uploads/file.png')
+        return "File uploaded"
+    else:
+        page = '''
+         <html>
+         <body>
+            <form action="" method="post" name="form" enctype="multipart/form-data">
+                <input type="file" name="datafile"/>
+                <input type="submit" name="submit" id="submit"/>
+            </form>
+        </body>
+        <html>
+        '''
+
+        return page, 200
